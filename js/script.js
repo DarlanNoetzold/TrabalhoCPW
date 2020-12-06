efa9fcbe6900e5aea902ac19bd4116570aae0db8
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2020-12-06 15:07:45 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+document.querySelector("#arAlun").style.display = 'none';
+document.querySelector("#arProf").style.display = 'none';
+
+function Mudarestado(el) {
+    var display = document.querySelector(el).style.display;
+    if(display == 'none')
+        document.getElementById(el).style.display = 'block';
+        alert("teste");
+}
+
 function enviaEmail(){
     var nome = document.querySelector(".nomePagPrin").value;
 
@@ -6,11 +16,13 @@ function enviaEmail(){
 
 function createFerEd(){
     var checkboxAl = document.querySelector("#checkAL");
+    var checkboxPro = document.querySelector("#checkPro");
     if(checkboxAl.checked == true) {
-        
+        Mudarestado('#arAluno');
+        alert("asda");
     }
     if(checkboxPro.checked == true) {
+        Mudarestado('#arProf');
         
     }
-
 }
