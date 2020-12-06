340246e56d6faa930f7d01078c7132d23b5a7a14
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2020-12-06 12:07:53 -0300

    .

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function enviaEmail(){
+    var nome = document.querySelector(".nomePagPrin").value;
+
+    alert("Você foi cadastrado " + nome);
+}
+
+function createFerEd(){
+    var checkboxAl = document.querySelector("#checkAL");
+    if(checkboxAl.checked == true) {
+        
+    }
+    if(checkboxPro.checked == true) {
+        
+    }
+
+}
