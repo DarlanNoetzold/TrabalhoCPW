524ea7dd0c9c5193ad8a5e653643b44772d14043
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2020-12-08 14:19:08 -0300

    .

--- js/script.js
+++ js/script.js
@@ -3,11 +3,15 @@ document.querySelector("#arProf").style.display = 'none';
 var prova1 = new Array(0,0,0,0,0,0,0,0,0,0,0);
 var prova2 = new Array(0,0,0,0,0,0,0,0,0,0,0);
 var prova3 = new Array(0,0,0,0,0,0,0,0,0,0,0);
+
+
 function enviaEmail() {
     var nome = document.querySelector(".nomePagPrin").value;
 
     alert("Você foi cadastrado " + nome);
 }
+
+
 var btnForm = document.querySelector("#btnForm");
 btnForm.onclick = function () {
     var checkboxAl = document.getElementById("checkAL");
@@ -25,13 +29,14 @@ btnForm.onclick = function () {
     return false;
 }
 
+
 var btnNotAl = document.querySelector("#btnNotAl");
 btnNotAl.onclick = function () {
     var txt = "";
     for (var i = 0; i < 10; i++) {
         var txt = txt + "<table border=\"1px\"><tr><td>Alunos</td><td>Prova 1</td><td>Prova 2</td><td>Prova 3</td><td>Média</td></tr>";
         for (var i = 1; i <= 10; i++) {
-            txt = txt + "<tr><td>Aluno " + i + "</td><td>" + prova1[i] + "</td><td>" + prova2[i] + "</td><td>" + prova3[i] + "</td><td>" + ((prova1[i] + prova2[i] + prova3[i]) / 3) + "</td></tr>";
+            txt = txt + "<tr><td>Aluno " + i + "</td><td>" + prova1[i] + "</td><td>" + prova2[i] + "</td><td>" + prova3[i] + "</td><td>" + (((prova1[i] + prova2[i] + prova3[i]) / 3).toFixed(2)) + "</td></tr>";
         }
         txt = txt + "</table><br>";
     }
@@ -39,12 +44,13 @@ btnNotAl.onclick = function () {
     return false;
 }
 
+
 var btnNotProf = document.querySelector("#btnNotProf");
 btnNotProf.onclick = function () {
     var i = document.querySelector('#opcoes').value;
-    var prova1i = parseInt(document.querySelector("#prova1i").value);
-    var prova2i = parseInt(document.querySelector("#prova2i").value);
-    var prova3i = parseInt(document.querySelector("#prova3i").value);
+    var prova1i = parseFloat(document.querySelector("#prova1i").value);
+    var prova2i = parseFloat(document.querySelector("#prova2i").value);
+    var prova3i = parseFloat(document.querySelector("#prova3i").value);
     prova1[i] = prova1i;
     prova2[i] = prova2i;
     prova3[i] = prova3i;
